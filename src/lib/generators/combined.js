bbdd75d702ad4ab212d9ec99d680774aa0733a51
Antonis Karamitros <antouankar@gmail.com>
2019-01-30 21:28:33 +0200

    pass second test. simple iteration

--- src/lib/generators/combined.js
+++ src/lib/generators/combined.js
@@ -2,6 +2,14 @@
 const fibonacci = require("./fibonacci");
 const triangle = require("./triangle");
 
-function* combined() {}
+const isIterable = iterable => Symbol.iterator in Object(iterable);
 
-module.exports = combined;
+function* combined(iterable) {
+  if (isIterable(iterable)) {
+    for (let value of iterable) {
+      yield value;
+    }
+  }
+}
+
+export default combined;
